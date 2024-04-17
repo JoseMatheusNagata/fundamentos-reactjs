66d6359e5b80dffda463bce1d14b28e2e769b72c
Jose <matheusjapa2010@hotmail.com>
2024-04-17 10:01:59 -0300

    propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,30 @@ const posts = [
   {
     id: 1,
     author: {
-      avatarUrl: 'https://github.com/diego3g.png',
-      name: 'Diego Fernandes',
-      role: 'CTO @Rocketseat'
+      avatarUrl: 'https://github.com/JoseMatheusNagata.png',
+      name: 'José Matheus Nagata Kulibaba',
+      role: 'CTO'
     },
     content: [
       { type: 'paragraph', content: 'Fala galera 👋' },
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2022-05-03 20:00:00'),
+    publishedAt: new Date('2024-04-03 20:00:00'),
   },
   {
     id: 2,
     author: {
-      avatarUrl: 'https://github.com/maykbrito.png',
-      name: 'Mayk Brito',
-      role: 'Educator @Rocketseat'
+      avatarUrl: 'https://github.com/JoseMatheusNagata.png',
+      name: 'José Matheus Nagata Kulibaba',
+      role: 'Educator'
     },
     content: [
       { type: 'paragraph', content: 'Fala galera 👋' },
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2022-05-10 20:00:00'),
+    publishedAt: new Date('2024-04-18 08:00:00'),
   },
 ];
 
