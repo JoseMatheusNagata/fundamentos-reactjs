26a0c7dc3a1efc23467ede33ad2f7b7f15b73d84
Jose <matheusjapa2010@hotmail.com>
2024-04-24 12:43:10 -0300

    Os motores do React - Validando formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,8 +31,14 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
     }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
+    }
+
 
     function deleteComment(commentToDelete) {
         const commentsWithoutDeletedOne = comments.filter(comment => {
@@ -42,6 +48,7 @@ export function Post({ author, publishedAt, content }) {
         setComments(commentsWithoutDeletedOne);
     }
 
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={style.post}>
@@ -78,9 +85,13 @@ export function Post({ author, publishedAt, content }) {
                     placeholder="Deixe um comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
                 <footer>
-                    <button type="submit">Comentar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Comentar
+                    </button>
                 </footer>
             </form>
             <div className={style.commentList}>
